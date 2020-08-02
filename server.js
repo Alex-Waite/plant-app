69a4b97fd375c395a3d004073a4e576dbb1550f5
alex-waite <alexanderconorwaite@gmail.com>
2020-08-03 00:06:15 +0930

    package saving

--- server.js
+++ server.js
@@ -14,7 +14,7 @@ if (process.env.NODE_ENV === "production") {
 // Add routes, both API and view
 app.use(routes);
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglist");
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/plantdb");
 
 // Start the API server
 app.listen(PORT, function() {
